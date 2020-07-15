54623764e3766f98420ab34c9b25e859bbd47a2d
JisooJang <ppuagirls@naver.com>
2020-07-16 02:02:35 +0900

    feat: implements handle method in AlarmTalkListener using feignClients method.

--- src/main/java/com/example/notification/listener/AlarmTalkListener.java
+++ src/main/java/com/example/notification/listener/AlarmTalkListener.java
@@ -1,13 +1,32 @@
 package com.example.notification.listener;
 
+import com.example.notification.client.SMSClient;
+import com.example.notification.choices.CountryCode;
+import com.example.notification.request.SendSMSRequest;
+import com.example.notification.response.SendSMSResponse;
 import com.example.notification.template.AlarmTalk;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
+@PropertySource("classpath:api-keys.properties")
 public class AlarmTalkListener {
+    private final SMSClient smsClient;
+    @Value("${toast.sms.app_key}")
+    private String appKey;
+
+    @Value("${toast.sms.sendNo}")
+    private String sendNo;
+
+    @Autowired
+    public AlarmTalkListener(SMSClient smsClient) {
+        this.smsClient = smsClient;
+    }
 
     @KafkaListener(topics = "alarmtalk.notification",
             containerFactory = "alarmTalkKafkaListenerContainerFactory",
@@ -15,5 +34,14 @@ public class AlarmTalkListener {
     )
     public void handle(AlarmTalk alarmTalk) {
         log.info("alarmTalkListener : alarmtalk.notification event received. : " + alarmTalk.getMediaId() + " : " + alarmTalk.getPhoneNumber());
+        SendSMSRequest request = SendSMSRequest.builder()
+                .body("문자 전송 test")
+                .sendNo(sendNo)
+                .build();
+        request.addRecipientInfo(alarmTalk.getPhoneNumber(), CountryCode.SOUTH_KOREA);
+
+        SendSMSResponse response = smsClient.sendSMS(appKey, request);
+        System.out.println(response);
+
     }
 }
