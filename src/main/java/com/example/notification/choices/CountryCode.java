84a1c2f43bdeb1e0a10d69647b8d32198ca40203
JisooJang <ppuagirls@naver.com>
2020-07-16 02:00:39 +0900

    feat: add CountryCode class in choices package

---
+++ src/main/java/com/example/notification/choices/CountryCode.java
@@ -0,0 +1,9 @@
+package com.example.notification.choices;
+
+public class CountryCode {
+    public static String SOUTH_KOREA = "82";
+
+    public CountryCode() throws Exception {
+        throw new Exception("cannot construct this class!");
+    }
+}
